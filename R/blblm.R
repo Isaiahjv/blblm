396aa2cabf8dfc2c167bfcc352cd62c718d1ab64
isaiahjv <isaiahjv96@gmail.com>
2020-06-11 05:43:28 -0700

    done

--- R/blblm.R
+++ R/blblm.R
@@ -1,6 +1,13 @@
 #' @import purrr
+#' @import furrr
 #' @import stats
+#' @import future
+#' @import parallel
+#' @import MASS
+#' @import utils
 #' @importFrom magrittr %>%
+#' @aliases NULL
+#' @export blblm
 #' @details
 #' Linear Regression with Little Bag of Bootstraps
 "_PACKAGE"
@@ -11,40 +18,97 @@
 utils::globalVariables(c("."))
 
 
+
+#' Retruns model fitted with bootstrap sampling
+#'
+#' @param formula model specification of response and predictor variables.
+#'
+#' @param data dataset to fit
+#' @param m integer
+#' @param B integer
+#' @param model linear model
+#' @param para logical indicating parallel computing
+#' @param core integer number of cores for paralle
+#'
 #' @export
-blblm <- function(formula, data, m = 10, B = 5000) {
+blblm <- function(formula, data, m = 10, B = 5000, model = 'lr', para = FALSE, core = 2) {
+  #browser()
+  if (para == FALSE){
   data_list <- split_data(data, m)
   estimates <- map(
     data_list,
-    ~ lm_each_subsample(formula = formula, data = ., n = nrow(data), B = B))
+    ~ lm_each_subsample(formula = formula, data = .,model ,n = nrow(data), B = B))
   res <- list(estimates = estimates, formula = formula)
   class(res) <- "blblm"
   invisible(res)
+
+  } else {
+    suppressWarnings(future::plan(future::multiprocess, workers = core))
+    data_list <- split_data(data, m)
+    estimates <-  data_list %>% furrr::future_map(function(dat){
+      lm_each_subsample(formula = formula, data = dat, model, n = nrow(data), B = B)
+    })
+    res <- list(estimates = estimates, formula = formula)
+    class(res) <- "blblm"
+    invisible(res)
+  }
 }
 
 
-#' split data into m parts of approximated equal sizes
+# split data into m parts of approximated equal sizes
 split_data <- function(data, m) {
   idx <- sample.int(m, nrow(data), replace = TRUE)
   data %>% split(idx)
 }
 
 
-#' compute the estimates
-lm_each_subsample <- function(formula, data, n, B) {
-  replicate(B, lm_each_boot(formula, data, n), simplify = FALSE)
+# compute the estimates
+lm_each_subsample <- function(formula, data,model,n, B) {
+  replicate(B, lm_each_boot(formula, data, n, model), simplify = FALSE)
 }
 
 
-#' compute the regression estimates for a blb dataset
-lm_each_boot <- function(formula, data, n) {
+# compute the regression estimates for a blb dataset
+lm_each_boot <- function(formula, data, n, model = 'lr') {
   freqs <- rmultinom(1, n, rep(1, nrow(data)))
-  lm1(formula, data, freqs)
+  if (model == 'lr'){
+    lm1(formula, data, freqs)
+  }
+  else if (model == 'poisson'){
+    lm2(formula, data, freqs)
+  }
+
+  else if(model == 'neg'){
+    lm3(formula, data, freqs)
+  }
+
+  else{
+    "Not an option"
+  }
+
 }
 
+# estimate the negative bionomial regression estimates based on given the number of repetitions
+lm3 <- function(formula,data,freqs){
 
-#' estimate the regression estimates based on given the number of repetitions
+  environment(formula) <- environment()
+  fit <- MASS::glm.nb(formula, data = data,weights = freqs)
+  list(coef = blbcoef(fit), sigma = blbsigma(fit))
+
+}
+
+# estimate the poisson regression estimates based on given the number of repetitions
+lm2 <- function(formula,data,freqs){
+
+  environment(formula) <- environment()
+  fit <- glm(formula,data,family="poisson",freqs)
+  list(coef = blbcoef(fit), sigma = blbsigma(fit))
+
+}
+
+# estimate the linear regression estimates based on given the number of repetitions
 lm1 <- function(formula, data, freqs) {
+
   # drop the original closure of formula,
   # otherwise the formula will pick a wront variable from the global scope.
   environment(formula) <- environment()
@@ -54,12 +118,16 @@ lm1 <- function(formula, data, freqs) {
 
 
 #' compute the coefficients from fit
+#'
+#' @param fit linear model
 blbcoef <- function(fit) {
   coef(fit)
 }
 
 
 #' compute sigma from fit
+#'
+#' @param fit linear model
 blbsigma <- function(fit) {
   p <- fit$rank
   y <- model.extract(fit$model, "response")
@@ -69,6 +137,12 @@ blbsigma <- function(fit) {
 }
 
 
+#' prints output for model
+#'
+#' @param x linear model
+#'
+#' @param ... so on
+#'
 #' @export
 #' @method print blblm
 print.blblm <- function(x, ...) {
@@ -77,9 +151,22 @@ print.blblm <- function(x, ...) {
 }
 
 
+#' returns bootstrapped estimates of sigma (variance) with optional confidence interval. Parallel computing is also optional
+#'
+#' @param object linear model
+#'
+#' @param confidence logical indicating confidence interval
+#' @param level numeric indicating level of confidence
+#' @param para logical indicating parallel computing
+#' @param core integer indicating number of cores for parallel computing
+#' @param ... so on
+#'
 #' @export
 #' @method sigma blblm
-sigma.blblm <- function(object, confidence = FALSE, level = 0.95, ...) {
+sigma.blblm <- function(object, confidence = FALSE,
+                        level = 0.95, para  = FALSE, core = 4 ,...) {
+
+  if(para == FALSE){
   est <- object$estimates
   sigma <- mean(map_dbl(est, ~ mean(map_dbl(., "sigma"))))
   if (confidence) {
@@ -91,46 +178,135 @@ sigma.blblm <- function(object, confidence = FALSE, level = 0.95, ...) {
   } else {
     return(sigma)
   }
+  }
+  else{
+    suppressWarnings(future::plan(future::multiprocess, workers = core))
+    est <- object$estimates
+    sigma <- mean(furrr::future_map_dbl(est, ~ mean(furrr::future_map_dbl(., "sigma"))))
+    if (confidence) {
+      alpha <- 1 - 0.95
+      limits <- est %>%
+        pmap_mean(~ quantile(furrr::future_map_dbl(., "sigma"), c(alpha / 2, 1 - alpha / 2)),core) %>%
+        set_names(NULL)
+      return(c(sigma = sigma, lwr = limits[1], upr = limits[2]))
+    } else {
+      return(sigma)
+    }
+  }
 }
 
+
+#' Returns bootstrapped estimates of paramter coefficients. Parallel computing is optional.
+#'
+#' @param object linear model
+#'
+#' @param para logical indicating parallel computing
+#' @param core integer indiciating number of cores
+#' @param ... so on
 #' @export
 #' @method coef blblm
-coef.blblm <- function(object, ...) {
+
+coef.blblm <- function(object, para = FALSE, core = 2, ...) {
   est <- object$estimates
-  map_mean(est, ~ map_cbind(., "coef") %>% rowMeans())
+  if(para == FALSE){
+    map_mean(est, ~ map_cbind(., "coef") %>% rowMeans())
+  }
+  else{
+    pmap_mean(est, ~ map_cbind(., "coef") %>% rowMeans(),core)
+  }
+
 }
 
-
+#' Returns confidence intervals for estimated parameter coefficients. Parallel computing is optional.
+#'
+#' @param object linear model
+#'
+#' @param parm parameter for confidence interval
+#' @param level numeric indicating level of confidence
+#' @param para logical indicating  parallel computing
+#' @param core integer indicating number of cores in parallel computing
+#' @param ... so on
+#'
 #' @export
 #' @method confint blblm
-confint.blblm <- function(object, parm = NULL, level = 0.95, ...) {
-  if (is.null(parm)) {
-    parm <- attr(terms(fit$formula), "term.labels")
+confint.blblm <- function(object, parm = NULL, level = 0.95, para = FALSE, core = 2, ...) {
+
+  if(para == FALSE){
+    if (is.null(parm)) {
+      parm <- attr(terms(object$formula), "term.labels")
+    }
+    alpha <- 1 - level
+    est <- object$estimates
+    out <- map_rbind(parm, function(p) {
+      map_mean(est, ~ map_dbl(., list("coef", p)) %>% quantile(c(alpha / 2, 1 - alpha / 2)))
+    })
+    if (is.vector(out)) {
+      out <- as.matrix(t(out))
+    }
+    dimnames(out)[[1]] <- parm
+    out
   }
-  alpha <- 1 - level
-  est <- object$estimates
-  out <- map_rbind(parm, function(p) {
-    map_mean(est, ~ map_dbl(., list("coef", p)) %>% quantile(c(alpha / 2, 1 - alpha / 2)))
-  })
-  if (is.vector(out)) {
-    out <- as.matrix(t(out))
+
+  else{
+    suppressWarnings(future::plan(future::multiprocess, workers = core))
+    if (is.null(parm)) {
+      parm <- attr(terms(object$formula), "term.labels")
+    }
+    alpha <- 1 - level
+    est <- object$estimates
+    out <- pmap_rbind(parm, function(p) {
+      pmap_mean(est, ~ furrr::future_map_dbl(., list("coef", p)) %>% quantile(c(alpha / 2, 1 - alpha / 2)))
+    })
+    if (is.vector(out)) {
+      out <- as.matrix(t(out))
+    }
+    dimnames(out)[[1]] <- parm
+    out
+
   }
-  dimnames(out)[[1]] <- parm
-  out
+
+
 }
 
+#' Returns predictions from fitted boostrap model on test data (data unseen by the fitted model). Parallel computing os optional.
+#'
+#' @param object linear model
+#'
+#' @param new_data dataset used for prediction
+#' @param confidence logical indicating confidence interval usage
+#' @param level numeric indicating level of confidence. see previous.
+#' @param para logical indicating parallel cimouting usage
+#' @param core integer indicating number of core in parallel computing
+#' @param ... so on
+#'
 #' @export
 #' @method predict blblm
-predict.blblm <- function(object, new_data, confidence = FALSE, level = 0.95, ...) {
-  est <- object$estimates
-  X <- model.matrix(reformulate(attr(terms(object$formula), "term.labels")), new_data)
-  if (confidence) {
-    map_mean(est, ~ map_cbind(., ~ X %*% .$coef) %>%
-      apply(1, mean_lwr_upr, level = level) %>%
-      t())
-  } else {
-    map_mean(est, ~ map_cbind(., ~ X %*% .$coef) %>% rowMeans())
+predict.blblm <- function(object, new_data, confidence = FALSE, level = 0.95, para = FALSE, core = 2, ...) {
+
+  if(para == FALSE){
+    est <- object$estimates
+    X <- model.matrix(reformulate(attr(terms.formula(object$formula,data = new_data), "term.labels")), new_data)
+    if (confidence) {
+      map_mean(est, ~ map_cbind(., ~ X %*% .$coef) %>%
+                 apply(1, mean_lwr_upr, level = level) %>%
+                 t())
+    } else {
+      map_mean(est, ~ map_cbind(., ~ X %*% .$coef) %>% rowMeans())
+    }
+  } else{
+    suppressWarnings(future::plan(future::multiprocess, workers = core))
+    est <- object$estimates
+    X <- model.matrix(reformulate(attr(terms.formula(object$formula,data = new_data), "term.labels")), new_data)
+    if (confidence) {
+      pmap_mean(est, ~ pmap_cbind(., ~ X %*% .$coef,core) %>%
+                 apply(1, mean_lwr_upr, level = level) %>%
+                 t(),core)
+    } else {
+      pmap_mean(est, ~ pmap_cbind(., ~ X %*% .$coef, core) %>% rowMeans(),core)
+    }
+
   }
+
 }
 
 
@@ -149,4 +325,20 @@ map_cbind <- function(.x, .f, ...) {
 
 map_rbind <- function(.x, .f, ...) {
   map(.x, .f, ...) %>% reduce(rbind)
+}
+
+pmap_mean <- function(.x, .f, core = 2, ...) {
+  suppressWarnings(future::plan(future::multiprocess, workers = core))
+  (furrr::future_map(.x, .f, ...) %>% reduce(`+`)) / length(.x)
+}
+
+pmap_cbind <- function(.x, .f,core = 2, ...) {
+
+  suppressWarnings(future::plan(future::multiprocess, workers = core))
+  furrr::future_map(.x, .f, ...) %>% reduce(cbind)
+}
+
+pmap_rbind <- function(.x, .f, core = 2, ...) {
+  suppressWarnings(future::plan(future::multiprocess, workers = core))
+  furrr::future_map(.x, .f, ...) %>% reduce(rbind)
 }
---
+++ tests/testthat/test-blblm.R
@@ -0,0 +1,24 @@
+test_that("type test", {
+
+  data1 = data.frame(x = rnorm(100,20,5), z = rnorm(100,80,9), y = rpois(100,4))
+  data2 = data.frame(x = rnorm(100,20,5), z = rnorm(100,80,9), y = rpois(100,4))
+
+  m = blblm(y~., data = data1, model = 'poisson', m = 3, B = 100, core = 1, para = TRUE)
+  coef = blbcoef(m)
+
+  expect_s3_class(m, "blblm")
+
+  #testing near equality with glm function. randomness pulls it away
+  expect_equal(coef(glm(y~.,data = data1, family = 'poisson')), coef.blblm(m),tolerance=.6)
+
+  #testing near equality of prediction
+  expect_equal(predict(glm(y~.,data = data1, family = 'poisson'),data2),predict.blblm(m,data2),tolerance=.6)
+
+
+
+
+
+
+
+
+})
